e473e4907dbb1299ebc2bf263e7b0bf5db101a8c
clark-pain <53712294+clark-pain@users.noreply.github.com>
2020-03-02 12:24:25 -0800

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,84 @@
+
+
+
+// What to do when click is detected
+var numberDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+
+// Detecting Key Press
+  document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+  });
+
+
+    function makeSound(key) {
+    switch (key) {
+      case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+        break;
+
+      case "a":
+      var kickbass = new Audio("sounds/kickbass.mp3");
+      kickbass.play();
+        break;
+
+      case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+        break;
+
+      case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+        break;
+
+
+      case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+        break;
+
+      case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+        break;
+
+      case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+        break;
+
+      default: console.log("no sound");
+
+    }
+
+  };
+
+  }
+
+  function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    }, 100);
+
+  }
